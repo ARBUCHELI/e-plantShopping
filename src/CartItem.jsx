a9ab484cfc33a5311fab84d7f752510bb0db4dc0
ANDRÉS R. BUCHELI (Andrés Fernando Regalado Bucheli) <radio_seattle@hotmail.com>
2026-02-04 19:26:54 +0300

    improving the alert

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -24,7 +24,7 @@ const CartItem = ({ onContinueShopping }) => {
   }
 
   const handleCheckoutShopping = e => {
-    alert('Functionality to be added for future reference')
+    alert('This is an educational project that is not integrated with a payment system, but the checkout functionality could be added for future reference')
   }
 
   const handleIncrement = item => {
