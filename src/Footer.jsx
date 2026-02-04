e9411de86066cec1d188edcec972600ed421fa75
ANDRÉS R. BUCHELI (Andrés Fernando Regalado Bucheli) <radio_seattle@hotmail.com>
2026-02-04 19:38:44 +0300

    creating footer and styling it

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="app-footer" role="contentinfo">
+      <p className="footer-text">
+        Andres R. Bucheli • IBM Professional Certification Initiative 2025
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
